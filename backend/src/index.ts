bc5aeac765464a06a06ffea93808571cb1a2f58c
miz29 <mizaanurrehman128@gmail.com>
2025-01-29 22:56:22 +0530

    corrected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+// Create the main Hono app
+const app = new Hono();
 
 app.post('/api/v1/signup', (c) => {
-  return c.text('signup route')
+	return c.text('signup route')
 })
 
 app.post('/api/v1/signin', (c) => {
 	return c.text('signin route')
 })
 
-app.get('api/v1/blog/:id', (c) => { 
-  return c.text('blog route')
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
 })
+
 app.post('/api/v1/blog', (c) => {
-  const id=c.req.param('id')
+
 	return c.text('signin route')
 })
 
